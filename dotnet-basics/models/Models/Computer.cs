830ecf7c9cd9dd6c8e68674f2b2d142a16919297
Petar Georgiev <petiyo07@abv.bg>
2024-04-13 02:50:06 +0300

    new section

---
+++ dotnet-basics/models/Models/Computer.cs
@@ -0,0 +1,18 @@
+namespace MyApp.Models
+{
+    public class Computer
+    {
+        public string Motherboard { get; set; } = "";
+        public int CPUCores { get; set; }
+
+        public bool HasWifi { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string VideoCard { get; set; } = "";
+
+    }
+}
+
+
---
+++ dotnet-basics/models/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MyApp.Models;
+
+namespace MyApp
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Computer myComputer = new()
+            {
+                Motherboard = "Test",
+                HasWifi = true,
+                ReleaseDate = DateTime.Now,
+                Price = 943.57m,
+                VideoCard = "RTX2060"
+            };
+
+            Console.WriteLine(myComputer.HasWifi);
+            myComputer.HasWifi = false;
+
+        }
+    }
+}
