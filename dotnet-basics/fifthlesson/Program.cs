ea82f3de34ba94c754d6cee61dd6ea769a968374
Petar Georgiev <petiyo07@abv.bg>
2024-04-18 13:22:40 +0300

    new lesson

---
+++ dotnet-basics/fifthlesson/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sql = @"
+                INSERT INTO TutorialAppSchema.Computer (
+                    Motherboard,
+                    HasWifi,
+                    HasLTE,
+                    ReleaseDate,
+                    Price,
+                    VideoCard 
+                ) VALUES ('""')";
+
+
+
+            File.WriteAllText("log.txt", sql);
+            // File.AppendAllText(Path.GetFullPath("log.txt"), sql);
+
+            using StreamWriter openFile = new("log.txt", append: true);
+
+            openFile.WriteLine(sql);
+        }
+    }
+}
